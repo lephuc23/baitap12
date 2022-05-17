006bc7d19c7dc48faab6940b32e0606e923e5b15
lephuc23 <96129033+lephuc23@users.noreply.github.com>
2022-05-17 09:07:15 +0700

    Add files via upload

---
+++ baitap/app/src/main/java/com/example/a2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.a2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button nutnhan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        nutnhan = findViewById(R.id.button);
+        nutnhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
